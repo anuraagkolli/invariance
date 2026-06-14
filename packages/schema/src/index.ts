8a590dfe43c01c10c368cc0373ab5edba030c987
Anuraag Kolli <anukolli05@gmail.com>
2026-06-13 22:22:30 -0700

    schema: DesignConfig (tunable look-invariants)

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,6 +1,7 @@
 // Browser-safe exports only. Node signing utilities live in
 // "@invariance/schema/signing" so importing the root never pulls node:crypto.
 export * from "./canonical-json";
+export { DesignConfigSchema, type DesignConfig } from "./design-config";
 export * from "./envelope";
 export * from "./manifest";
 export * from "./mod";
